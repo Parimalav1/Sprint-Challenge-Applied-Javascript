db424af0d3a09db508e38c56298845d1f2a61d3d
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-06-05 16:04:23 -0700

    stretch goal

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,53 @@
     <div class="right-button"> > </div>
   </div>
 */
+const fourthDiv = document.querySelector('.carousel-container');
+
+function carouselMaker(picsList) {
+  const carouselDiv = document.createElement('div');
+  const lfButton = document.createElement('div');
+  const imgDiv1 = document.createElement('img');
+  const imgDiv2 = document.createElement('img');
+  const rtButton = document.createElement('div');
+
+  carouselDiv.appendChild(lfButton);
+  carouselDiv.appendChild(imgDiv1);
+  carouselDiv.appendChild(imgDiv2);
+  carouselDiv.appendChild(rtButton);
+
+  carouselDiv.classList.add('carousel');
+  lfButton.classList.add('left-button');
+  rtButton.classList.add('right-button');
+  lfButton.textContent = '<';
+  rtButton.textContent = '>';
+
+  let currentIndex = 0;
+  imgDiv1.src = picsList[currentIndex % picsList.length];
+  imgDiv2.src = picsList[(currentIndex + 1) % picsList.length];
+
+  rtButton.addEventListener('click', event => {
+    currentIndex = currentIndex - 1;
+    if (currentIndex < 0) {
+      currentIndex = currentIndex + picsList.length;
+    }
+    imgDiv1.src = picsList[currentIndex % picsList.length];
+    imgDiv2.src = picsList[(currentIndex + 1) % picsList.length];
+    //console.log(`Now showing, currentIndex = ${currentIndex}, Images=${imgDiv1.src} and ${imgDiv2.src}`);
+    })
+  lfButton.addEventListener('click', event => {
+    currentIndex = currentIndex + 1;
+    imgDiv1.src = picsList[currentIndex % picsList.length];
+    imgDiv2.src = picsList[(currentIndex + 1) % picsList.length];
+    //console.log(`Now showing, currentIndex = ${currentIndex}, Images=${imgDiv1.src} and ${imgDiv2.src}`);
+    })
+  return carouselDiv;
+}
+const carouselpicsArray = [
+  'assets/carousel/mountains.jpeg',
+  'assets/carousel/computer.jpeg',
+  'assets/carousel/trees.jpeg',
+  'assets/carousel/turntable.jpeg'
+];
+
+const carousel = carouselMaker(carouselpicsArray);
+fourthDiv.appendChild(carousel);
